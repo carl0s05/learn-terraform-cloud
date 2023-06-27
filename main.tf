d4c7b93ca2ae5097a1fee3a8681bbe33b9f92ceb
carl0s05 <emilio.vazquez@coppel.com>
2023-06-26 18:33:35 -0600

    Ajustes en el codigo

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 
 # Configure the AWS Provider ESTO ES PRUEBA
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-east-2"
 }
 
 resource "aws_security_group" "GS_TEST_TF" {
